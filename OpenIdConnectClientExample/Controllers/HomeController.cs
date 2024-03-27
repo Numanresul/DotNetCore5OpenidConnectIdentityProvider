d91bdd4d83b3e0b9b8272b727272e40c95fd7252
NUMAN <numankul06@gmail.com>
2024-03-27 23:05:41 +0300

    Update HomeController.cs

--- OpenIdConnectClientExample/Controllers/HomeController.cs
+++ OpenIdConnectClientExample/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -7,6 +7,7 @@ using OpenIdConnectClientExample.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,8 +22,25 @@ namespace OpenIdConnectClientExample.Controllers
             _logger = logger;
         }
         [Authorize]        
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            // Token içeriğine erişim
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var idToken = await HttpContext.GetTokenAsync("id_token");
+
+            // Token içeriğini işleme
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(accessToken) as JwtSecurityToken;
+
+            // JSON içindeki claim'lere erişim
+           // var nameClaim = jsonToken.Claims.FirstOrDefault(c => c.Type == "name")?.Value;
+            var emailClaim = jsonToken.Claims.FirstOrDefault(c => c.Type == "email")?.Value;
+
+            // Claim'leri kullanma
+            // Örneğin, bu claim'leri bir view'e aktarabilir veya iş mantığınızda kullanabilirsiniz
+            //ViewBag["Name"] = nameClaim;
+            ViewBag.Email = emailClaim.ToString();
+
             return View();
         }
         [Authorize]
