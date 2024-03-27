877b1307bbc9131c8f70cf13b6e4c891dcfd00f2
NUMAN <numankul06@gmail.com>
2024-03-27 23:05:03 +0300

    Update Startup.cs

--- OpenIdConnectClientExample/Startup.cs
+++ OpenIdConnectClientExample/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,11 +7,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using IHostingEnvironment = Microsoft.Extensions.Hosting.IHostingEnvironment;
@@ -20,46 +24,82 @@ namespace OpenIdConnectClientExample
     public class Startup
     {
         public Startup(IConfiguration configuration)
-        {         
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(opt => opt.RequireHttpsPermanent = false);
-            
-           
-            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             services.AddControllersWithViews();
-            
-            services.AddAuthentication(_ =>
-        {
-            _.DefaultScheme = "numan";
-            _.DefaultChallengeScheme = "oidc";
-        })
-        .AddCookie("numan")
-        .AddOpenIdConnect("oidc", _ =>
-        {
-            _.SignInScheme = "numan";
-            _.Authority = "https://localhost:44394";
-            _.ClientId = "default-client";
-            _.ClientSecret = "379ea949-a2c9-4508-a51f-add1173af8c0";
-            _.ResponseType = "code";
-            _.ResponseMode = "query";
-            _.SaveTokens = true;
-        });
-           
-         
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "numan";
+                options.DefaultChallengeScheme = "oidc";
+            })
+            .AddCookie("numan")
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.SignInScheme = "numan";
+                options.Authority = "https://server-domain";
+                options.ClientId = "testoidc";
+                options.ClientSecret = "client_example";
+                options.ResponseType = "code";
+                options.ResponseMode = "query";
+                options.SaveTokens = true;
+
+                options.Scope.Clear(); // Clear existing scopes
+                options.Scope.Add("openid"); // Required
+                options.Scope.Add("profile"); // Optional
+                options.Scope.Add("email"); // Optional
+                options.Scope.Add("securify"); // Optional
+
+                options.MetadataAddress = "https://domain/.well-known/openid-configuration";
+
+                options.MetadataAddress = "https://domain/.well-known/openid-configuration";
+                options.Configuration = new Microsoft.IdentityModel.Protocols.OpenIdConnect.OpenIdConnectConfiguration
+                {
+                    JwksUri = "https://domain/.well-known/openid-configuration/jwks",
+                    Issuer = "https://domain",
+                    AuthorizationEndpoint = "domain/connect/authorize",
+                    TokenEndpoint = "https://domain/connect/token",
+                    UserInfoEndpoint = "https://domain/connect/userinfo",
+                    EndSessionEndpoint = "https://domain/connect/endsession",
+                    CheckSessionIframe = "https://domain/connect/checksession",
+                    IntrospectionEndpoint = "https://domain/connect/introspect",
+                };
+
+
+                if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+                {
+                    options.BackchannelHttpHandler = new HttpClientHandler
+                    {
+                        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                    };
+                }
+
+                // Set the TokenValidationParameters to bypass signature validation
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = false,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = false,
+                    SignatureValidator = delegate (string token, TokenValidationParameters parameters)
+                    {
+                        var jwt = new JwtSecurityToken(token);
+                        return jwt;
+                    },
+                };
+            });
         }
-          
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             IdentityModelEventSource.ShowPII = true;
-            
 
             if (env.IsDevelopment())
             {
@@ -68,9 +108,9 @@ namespace OpenIdConnectClientExample
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
